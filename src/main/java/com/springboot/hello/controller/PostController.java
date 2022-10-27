a77237f88e89af11bf2509620bc4daac5691bef5
cmkxak <gcm466@naver.com>
2022-10-27 14:11:55 +0900

    add : RequestBody와 Map을 활용한 POST API 구현

--- src/main/java/com/springboot/hello/controller/PostController.java
+++ src/main/java/com/springboot/hello/controller/PostController.java
@@ -1,16 +1,25 @@
 package com.springboot.hello.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/post-api")
 public class PostController {
 
-    @RequestMapping(value = "/domain", method = RequestMethod.POST)
+    @PostMapping(value = "/domain")
     public String postExample(){
         return "Hello Post API";
+    }
+
+    @PostMapping
+    //어떤 타입의 데이터 값이 들어올지 모르므로 Object 타입으로 value 지정
+    public String postMember(@RequestBody Map<String, Object> postData){
+        StringBuilder sb = new StringBuilder();
+        postData.entrySet().forEach(map->{
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+        return sb.toString();
     }
 }
