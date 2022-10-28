9cd973eb205b2f0eb6b9a0497193504c3c203321
cmkxak <gcm466@naver.com>
2022-10-28 11:34:47 +0900

    add : put API 예제 메소드 추가

--- src/main/java/com/springboot/hello/controller/PutController.java
+++ src/main/java/com/springboot/hello/controller/PutController.java
@@ -10,8 +10,28 @@ import java.util.Map;
 @RequestMapping("/api/v1/put-api")
 public class PutController {
 
+    @PutMapping("/member")
+    public String postMember(@RequestBody Map<String, Object> putData) {
+        StringBuilder sb = new StringBuilder();
+
+        putData.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " " + map.getValue() + "\n");
+        });
+        return sb.toString();
+    }
+
+    @PutMapping("/member1")
+    public String postMemberDto1(@RequestBody MemberDto memberDto) {
+        return memberDto.toString(); //문자열 반환
+    }
+
+    @PutMapping("/member2")
+    public MemberDto postMemberDto2(@RequestBody MemberDto memberDto) {
+        return memberDto; //DTO 타입의 오브젝트 반환
+    }
+
     @PutMapping(value = "/member3")
-    public ResponseEntity<MemberDto> postMemberDto3(@RequestBody MemberDto memberDto){
+    public ResponseEntity<MemberDto> postMemberDto3(@RequestBody MemberDto memberDto) {
         return ResponseEntity
                 .status(HttpStatus.ACCEPTED)
                 .body(memberDto);
