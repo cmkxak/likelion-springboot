8ea19a4d449e78bb18a8fa2731707119ee9acf6b
cmkxak <gcm466@naver.com>
2022-10-28 14:03:33 +0900

    add : 유저 기능, 전체 유저 삭제 기능, 1명의 유저 삭제 기능 추가

---
+++ src/main/java/com/springboot/hello/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.springboot.hello.controller;
+
+import com.springboot.hello.dao.UserDao;
+import com.springboot.hello.domain.User;
+import org.springframework.web.bind.annotation.*;
+
+import javax.sql.DataSource;
+import java.util.Map;
+
+@RestController
+public class UserController {
+    private UserDao userDao;
+
+    public UserController(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @PostMapping("/user")
+    public void save(@RequestBody User user){
+        userDao.add(user);
+    }
+
+    @DeleteMapping("/user/{id}")
+    public void deleteUser(@PathVariable String id){
+        userDao.delete(id);
+    }
+
+    @DeleteMapping("/user/all")
+    public void deleteUserAll(){
+        userDao.deleteAll();
+    }
+
+}
