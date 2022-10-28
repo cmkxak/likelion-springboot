0e82cc00528f1e66579f09a2fbc679c36854a171
cmkxak <gcm466@naver.com>
2022-10-28 13:48:07 +0900

    add : UserDao 추가

---
+++ src/main/java/com/springboot/hello/dao/UserDao.java
@@ -0,0 +1,31 @@
+package com.springboot.hello.dao;
+
+import com.springboot.hello.domain.User;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+
+@Repository
+public class UserDao {
+    private DataSource dataSource;
+    private JdbcTemplate jdbcTemlate;
+
+    public UserDao(DataSource dataSource, JdbcTemplate jdbcTemlate) {
+        this.dataSource = dataSource;
+        this.jdbcTemlate = jdbcTemlate;
+    }
+
+    public void add(User user) {
+        this.jdbcTemlate.update("insert into users values(?,?,?);", user.getId(), user.getName(), user.getPassword());
+    }
+
+    public void delete(String id) {
+        this.jdbcTemlate.update("delete from users where id = ?", id);
+    }
+
+    public void deleteAll() {
+        this.jdbcTemlate.update("delete from users;");
+    }
+
+}
