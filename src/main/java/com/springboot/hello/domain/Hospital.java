2568e43cb1dead21b8e9f715b644d95e7b687b6e
cmkxak <gcm466@naver.com>
2022-10-31 14:29:24 +0900

    add : hospital api 구축 1. domain 클래스 추가 2. Dao 클래스 추가 3. 파일 파싱 작업 추가 4. Test 코드 추가

---
+++ src/main/java/com/springboot/hello/domain/Hospital.java
@@ -0,0 +1,34 @@
+package com.springboot.hello.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class Hospital {
+    private int id;
+    private String openServiceName;
+    private int openLocalGovermentCode;
+    private String managementNumber;
+    private LocalDateTime licenseDate;
+    private int businessStatus;
+    private int businessStatusCode;
+    private String phone;
+    private String fullAddress;
+    private String roadNameAddress;
+    private String hospitalName;
+    private String businessTypeName;
+    private int healthcareProviderCount;
+    private int patientRoomCount;
+    private int totalNumberofBeds;
+    private float totalAreaSize;
+
+}
