864afe92b489ced08b0101c263e860b64f5ac806
cmkxak <gcm466@naver.com>
2022-11-01 13:43:49 +0900

    add : jdbcTemplate 도입 및 Insert 쿼리 추가

--- src/main/java/com/springboot/hello/dao/HospitalDao.java
+++ src/main/java/com/springboot/hello/dao/HospitalDao.java
@@ -1,4 +1,24 @@
 package com.springboot.hello.dao;
 
+import com.springboot.hello.domain.Hospital;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class HospitalDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    public HospitalDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void add(Hospital hospital) {
+        String sql = "INSERT INTO `likelion-db`, `hospitals` (`id`, `open_service_name`, `open_local_goverment_code`, `management_number`,`license_date`, `business_status`," +
+                "`business_status_code`, `phone`, `full_address`, `road_name_address`, `hospital_name`, `business_type_name`, `healthcare_provider_count`, `patient_room_count`"
+                + "`total_number_of_beds`, `total_area_size`)" +
+                "values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+
+        this.jdbcTemplate.update(sql, hospital.getId(), hospital.getOpenServiceName(), hospital.getOpenLocalGovermentCode(),
+                hospital.getManagementNumber(), hospital.getLicenseDate(), hospital.getBusinessStatus(), hospital.getBusinessStatusCode(), hospital.getPhone(), hospital.getFullAddress(),
+                hospital.getRoadNameAddress(), hospital.getHospitalName(), hospital.getBusinessTypeName(), hospital.getHealthcareProviderCount(),
+                hospital.getPatientRoomCount(), hospital.getTotalNumberofBeds(), hospital.getTotalAreaSize());
+    }
 }
