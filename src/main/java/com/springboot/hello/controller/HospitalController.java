7490f90d0af93ac044a4c8ee5c4584a5ebe24e5f
cmkxak <gcm466@naver.com>
2022-11-02 13:08:16 +0900

    refact : id를 조회하면, ResponseEntity를 반환하도록 수정

--- src/main/java/com/springboot/hello/controller/HospitalController.java
+++ src/main/java/com/springboot/hello/controller/HospitalController.java
@@ -2,10 +2,14 @@ package com.springboot.hello.controller;
 
 import com.springboot.hello.dao.HospitalDao;
 import com.springboot.hello.domain.Hospital;
+import com.springboot.hello.domain.User;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 
 @RestController
@@ -18,12 +22,11 @@ public class HospitalController {
     }
 
     @GetMapping("/{id}")
-    public String getHospital(@PathVariable int id) {
-        Hospital hospital = hospitalDao.findById(id);
-
-        return hospital.getHospitalName() + " " + hospital.getFullAddress() + " " + hospital.getRoadNameAddress() +
-                " " + hospital.getHealthcareProviderCount() + " " + hospital.getTotalNumberOfBeds() + " " + hospital.getTotalAreaSize(); //폐업 여부는 아직 구현하지 않음
+    public ResponseEntity<Hospital> getHospital(@PathVariable int id) {
+        Hospital selectedHospital = hospitalDao.findById(id);
+        return ResponseEntity
+                .ok()
+                .body(selectedHospital);
     }
-
 
 }
